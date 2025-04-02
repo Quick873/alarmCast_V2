26d20d4d4951257629cf1e60d4393bec93cb406f
Quick873 <116125610+Quick873@users.noreply.github.com>
2025-04-01 22:04:38 -0500

    Lots of comments

--- login.py
+++ login.py
@@ -3,13 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# I am not 100% sure what this does. 
+# This is used to validate browser cookies and to make sure they weren't tampered with. 
 app.secret_key = "Redbe@rd@5510"
 
 # Login Manager lets the application and Flask Login work together.
 login_manager = LoginManager()
 
-# I am not sure what this does yet. 
+# This registers Flask Login functionality with the app. 
 login_manager.init_app(app)
 
 # This will need to be more secure than this.
@@ -29,5 +29,9 @@ def load_user(user_id):
     return User.get(user_id)
 
 
-#This should route to the login page.
+#This will run the function when someone goes to the login URL
+# @app.route('/login) is the indication to run the function when someone enters the login url
+# methods=['GET', 'POST'] means this route handles both get and post request.
+# GET - displays the login form.
+# POST - Processes the form (validates credentials)
 @app.route('/login', methods=['GET', 'POST'])
